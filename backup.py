19537f6a0101e12227f9c80de1db3e72e303d4f6
helloJiu <hufengjiujiayou@163.com>
2018-11-19 16:54:59 +0800

    commit

--- backup.py
+++ backup.py
@@ -6,5 +6,4 @@ config = get_config(option.config_file)
 mysql_config = config['mysql']
 sql = 'show databases'
 cmd = 'mysql -h%s -P%s -u%s -p%s -e"%s"' % (mysql_config['host'], mysql_config['port'], mysql_config['username'],mysql_config['password'], sql)
-print(call_system_command(cmd))
-print('haha')
+print call_system_command(cmd)
